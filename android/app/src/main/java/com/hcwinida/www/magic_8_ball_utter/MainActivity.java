343cc462b346b97f105838b11e58c2009178491f
Allauddin Khan <ashrafjamkhane@gmail.com>
2021-07-17 19:19:05 +0530

    Initial Commit

---
+++ android/app/src/main/java/com/hcwinida/www/magic_8_ball_utter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hcwinida.www.magic_8_ball_utter;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
